800d1de16105567f46074e9b0d6a6357d3c5dbd6
Om Verma <76005616+omvermaa@users.noreply.github.com>
2026-02-03 18:47:09 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
-import Home from './components/home.jsx'
+import Home from './components/Home.jsx'
 import Login from './components/Login.jsx'
 import Signup from './components/Signup.jsx'
 import Profile from './components/Profile.jsx'
